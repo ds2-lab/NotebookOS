eebee728bff4831e25dcc2c5be2d11cc479e539f
Ben Carver <bcarver2@gmu.edu>
2024-07-23 23:04:48 +0000

    Got debug python working within Jupyter container

---
+++ dockfiles/cpu-python3-amd64-debug/start-notebook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Shim to emit warning and call start-notebook.py
+echo "WARNING: Use start-notebook.py instead"
+
+# exec /usr/local/bin/start-notebook.py "$@"
+/home/jovyan/Python-3.11.9/debug/python /usr/local/bin/start-notebook.py "$@"
