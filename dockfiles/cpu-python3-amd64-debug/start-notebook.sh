aa9297772188b81a7dbdd131e70baf74e1bef800
Ben Carver <bcarver2@gmu.edu>
2024-07-24 22:22:43 +0000

    More debugging

--- dockfiles/cpu-python3-amd64-debug/start-notebook.sh
+++ dockfiles/cpu-python3-amd64-debug/start-notebook.sh
@@ -4,3 +4,4 @@ echo "WARNING: Use start-notebook.py instead"
 
 # exec /usr/local/bin/start-notebook.py "$@"
 /home/jovyan/Python-3.11.9/debug/python /usr/local/bin/start-notebook.py "$@"
+# gdb -ex run --args /home/jovyan/Python-3.11.9/debug/python /usr/local/bin/start-notebook.py "$@"
