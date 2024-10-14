47f8a5f65293b9b32af333f0f94e5b9aa8604560
bcarver <bcarver2@gmu.edu>
2024-10-14 17:45:41 -0400

    Added helper script to make running ansible playbooks more convenient

---
+++ setup/ansible/run_playbook.py
@@ -0,0 +1,104 @@
+import argparse
+from ansible_runner import run
+from typing import Optional
+import yaml
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p", "--playbook", type = str, default = "ansible_setup.yml", help = "The name of the playbook to run.")
+parser.add_argument("-i", "--inventory-file", type = str, default = "./inventory_file.ini", help = "Path to the Ansible inventory file.")
+parser.add_argument("-v", "--vars-file", type = str, default = "./ansible_vars.yml", help = "Path to the Ansible 'vars' file.")
+parser.add_argument("-l", "--list-playbooks", action = "store_true", help = "List all possible playbooks.")
+
+args = parser.parse_args()
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[33m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# AllPlaybooks is a dictionary that maps "colloquial" names to the file.
+AllPlaybooks: dict[str, str] = {
+    "BuildSMR": "ansible_build_smr.yml",
+    "CloneRepo": "ansible_clone_repo.yml",
+    "InstallDocker": "ansible_install_docker.yml",
+    "InstallGit": "ansible_install_git.yml",
+    "InstallGolang": "ansible_install_golang.yml",
+    "InstallHadoopHdfs": "ansible_install_hadoop_hdfs.yml",
+    "InstallHelm": "ansible_install_helm.yml",
+    "InstallKubectl": "ansible_install_kubectl.yml",
+    "InstallProtoc": "ansible_install_protoc.yml",
+    "InstallPython": "ansible_install_python.yml",
+    "InstallPythonModule": "ansible_install_python_module.yml",
+    "AnsibleSetup": "ansible_setup.yml",
+}
+
+PlaybookFiles: set[str] = set(AllPlaybooks.values())
+
+def get_playbook_listing(colloquialName:str, playbookFile:str)->str:
+    """
+    Return the formatted listing to be output when listing playbooks for the given entry.
+    """
+    return (f"{bcolors.OKBLUE}{colloquialName}{bcolors.YELLOW}{bcolors.ENDC}\n\t"
+            f"{bcolors.HEADER}Playbook File{bcolors.ENDC}: {bcolors.YELLOW}\"{bcolors.OKCYAN}{playbookFile}{bcolors.YELLOW}\"{bcolors.ENDC}\n")
+
+if args.list_playbooks:
+    print(bcolors.HEADER + bcolors.BOLD + "Available Playbooks:" + bcolors.ENDC)
+
+    counter:int = 1
+    for colloquial_name, playbook_file in AllPlaybooks.items():
+        print(f"{counter}: {get_playbook_listing(colloquial_name, playbook_file)}")
+        counter += 1
+
+    exit(0)
+
+playbook: str = args.playbook
+inventory_file: str = args.inventory_file
+vars_file: str = args.vars_file
+
+print(f"Selected playbook \"{playbook}\".")
+print(f"Inventory file path: \"{inventory_file}\"")
+print(f"Ansible variables file path: \"{vars_file}\"")
+
+NewEntries: dict[str, str] = {}
+for key, value in AllPlaybooks.items():
+    # Add lowercase versions of the colloquial names as keys.
+    NewEntries[key.lower()] = value
+
+    # Add versions of the keys without "install" -- both in lowercase and non-lowercase.
+    key_without_prefix:str = key.removeprefix("Install")
+    NewEntries[key_without_prefix.lower()] = value
+    NewEntries[key_without_prefix] = value
+
+    # Map the filenames to themselves.
+    NewEntries[value] = value
+
+AllPlaybooks.update(NewEntries)
+
+playbook_file:Optional[str] = None
+
+print("Possible Keys:")
+print(list(AllPlaybooks.keys()))
+
+if playbook in AllPlaybooks or playbook.lower() in AllPlaybooks:
+    playbook_file = AllPlaybooks.get(playbook, AllPlaybooks[playbook.lower()])
+else:
+    raise ValueError(f"Unknown or unsupported playbook specified: \"{playbook}\"")
+
+with open(vars_file, "r") as f:
+    extra_vars = yaml.load(f, Loader=yaml.FullLoader)
+
+with open(inventory_file, "r") as f:
+    inventory_file:list[str] = f.readlines()
+    inventory_file_as_string:str = ""
+    for line in inventory_file:
+        inventory_file_as_string += line
+
+run(playbook = playbook_file, extravars = extra_vars, private_data_dir = "./", inventory = inventory_file_as_string)
