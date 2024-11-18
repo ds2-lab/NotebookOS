1022ab0862960d501890fbbbe8dec7c5a42582a5
bcarver <bcarver2@gmu.edu>
2024-11-18 18:24:07 -0500

    Added RemoteStorageDefinition to ExecuteRequestMetadata

--- common/jupyter/messaging/message.go
+++ common/jupyter/messaging/message.go
@@ -435,6 +435,10 @@ type ExecuteRequestMetadata struct {
 	// replica would execute the code. This is only sent on the return (i.e., "execute_reply").
 	ElectionMetadata *ElectionMetadata `json:"election_metadata" mapstructure:"resource_snapshot,omitempty"`
 
+	// RemoteStorageDefinition defines the remote storage that should be used by the kernel when simulating
+	// checkpointing its state.
+	RemoteStorageDefinition *proto.RemoteStorageDefinition `json:"remote_storage_definition" mapstructure:"remote_storage_definition"`
+
 	// OtherMetadata contains any other entries in the metadata frame that aren't explicitly listed above.
 	// OtherMetadata will only be populated if the metadata frame is decoded using the mapstructure library.
 	OtherMetadata map[string]interface{} `mapstructure:",remain"`
