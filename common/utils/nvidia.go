df876f079563f7d6f0bb13d4b8c37374e25212fc
bcarver <bcarver2@gmu.edu>
2024-12-18 11:57:59 -0500

    Commented-out automatic GPU resolution bc it won't build in the Docker container...

--- common/utils/nvidia.go
+++ common/utils/nvidia.go
@@ -1,9 +1,9 @@
 package utils
 
-import (
-	"fmt"
-	"github.com/NVIDIA/go-nvml/pkg/nvml"
-)
+//import (
+//	"fmt"
+//	"github.com/NVIDIA/go-nvml/pkg/nvml"
+//)
 
 // GetNumberOfActualGPUs attempts to use the [Go Bindings for the NVIDIA Management Library] to retrieve the number of
 // real/actual GPUs available on the host.
@@ -14,22 +14,23 @@ import (
 //
 // [Go Bindings for the NVIDIA Management Library]: https://github.com/NVIDIA/go-nvml?tab=readme-ov-file#quick-start
 func GetNumberOfActualGPUs() (int, error) {
-	ret := nvml.Init()
-	if ret != nvml.SUCCESS { // Official docs for nvml go module do not use errors.Is or errors.As here
-		return -1, fmt.Errorf("unable to initialize NVML: %v", nvml.ErrorString(ret))
-	}
-
-	defer func() {
-		ret := nvml.Shutdown()
-		if ret != nvml.SUCCESS { // Official docs for nvml go module do not use errors.Is or errors.As here
-			panic(fmt.Sprintf("Unable to shutdown NVML: %v", nvml.ErrorString(ret)))
-		}
-	}()
-
-	count, ret := nvml.DeviceGetCount()
-	if ret != nvml.SUCCESS { // Official docs for nvml go module do not use errors.Is or errors.As here
-		return -1, fmt.Errorf("unable to get device count: %v", nvml.ErrorString(ret))
-	}
-
-	return count, nil
+	panic("Had to comment this out because building was failing and I don't feel like debugging that right now")
+	//ret := nvml.Init()
+	//if ret != nvml.SUCCESS { // Official docs for nvml go module do not use errors.Is or errors.As here
+	//	return -1, fmt.Errorf("unable to initialize NVML: %v", nvml.ErrorString(ret))
+	//}
+	//
+	//defer func() {
+	//	ret := nvml.Shutdown()
+	//	if ret != nvml.SUCCESS { // Official docs for nvml go module do not use errors.Is or errors.As here
+	//		panic(fmt.Sprintf("Unable to shutdown NVML: %v", nvml.ErrorString(ret)))
+	//	}
+	//}()
+	//
+	//count, ret := nvml.DeviceGetCount()
+	//if ret != nvml.SUCCESS { // Official docs for nvml go module do not use errors.Is or errors.As here
+	//	return -1, fmt.Errorf("unable to get device count: %v", nvml.ErrorString(ret))
+	//}
+	//
+	//return count, nil
 }
