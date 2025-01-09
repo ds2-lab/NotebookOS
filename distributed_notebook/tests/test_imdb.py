1cbee945064c8c0c26dac644c257f38042888235
bcarver <bcarver2@gmu.edu>
2025-01-09 15:07:22 -0500

    Updated IMDb tests

--- distributed_notebook/tests/test_imdb.py
+++ distributed_notebook/tests/test_imdb.py
@@ -3,24 +3,24 @@ import os
 
 from distributed_notebook.datasets import IMDbLargeMovieReview
 
-def test_cola_download_fresh_with_bert():
+def test_imdb_download_fresh_with_bert():
     if os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb"):
         shutil.rmtree("~/.cache/huggingface/datasets/stanfordnlp___imdb")
 
     if os.path.isdir("~/tokenized_datasets/glue/bert"):
         shutil.rmtree("~/tokenized_datasets/glue/bert")
 
-    cola_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
+    imdb_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert not cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert not imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
     assert os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb")
     assert os.path.isdir("~/tokenized_datasets/glue/bert")
 
-def test_cola_download_fresh_with_gpt2():
+def test_imdb_download_fresh_with_gpt2():
     if os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb"):
         shutil.rmtree("~/.cache/huggingface/datasets/stanfordnlp___imdb")
 
@@ -30,29 +30,29 @@ def test_cola_download_fresh_with_gpt2():
     if os.path.isdir("~/tokenized_datasets/glue/gpt2"):
         shutil.rmtree("~/tokenized_datasets/glue/gpt2")
 
-    cola_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "gpt-2")
+    imdb_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "gpt-2")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert not cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert not imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
     assert os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb")
     assert os.path.isdir("~/tokenized_datasets/glue/gpt-2")
 
-def test_create_cola_already_downloaded_bert():
+def test_create_imdb_already_downloaded_bert():
     if os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb"):
         shutil.rmtree("~/.cache/huggingface/datasets/stanfordnlp___imdb")
 
     if os.path.isdir("~/tokenized_datasets/glue/bert"):
         shutil.rmtree("~/tokenized_datasets/glue/bert")
 
-    cola_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
+    imdb_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert not cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert not imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
     assert os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb")
     assert os.path.isdir("~/tokenized_datasets/glue/bert")
@@ -61,50 +61,50 @@ def test_create_cola_already_downloaded_bert():
     if os.path.isdir("~/tokenized_datasets/glue/bert"):
         shutil.rmtree("~/tokenized_datasets/glue/bert")
 
-    cola_dataset = IMDbLargeMovieReview(model_name = "bert")
+    imdb_dataset = IMDbLargeMovieReview(model_name = "bert")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
-def test_create_cola_already_downloaded_and_tokenized_bert():
+def test_create_imdb_already_downloaded_and_tokenized_bert():
     if os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb"):
         shutil.rmtree("~/.cache/huggingface/datasets/stanfordnlp___imdb")
 
     if os.path.isdir("~/tokenized_datasets/glue/bert"):
         shutil.rmtree("~/tokenized_datasets/glue/bert")
 
-    cola_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
+    imdb_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert not cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert not imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
     assert os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb")
     assert os.path.isdir("~/tokenized_datasets/glue/bert")
 
-    cola_dataset = IMDbLargeMovieReview(model_name = "bert")
+    imdb_dataset = IMDbLargeMovieReview(model_name = "bert")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert cola_dataset.dataset_already_downloaded
-    assert cola_dataset.dataset_already_tokenized
+    assert imdb_dataset.dataset_already_downloaded
+    assert imdb_dataset.dataset_already_tokenized
 
-def test_create_cola_already_downloaded_gpt2():
+def test_create_imdb_already_downloaded_gpt2():
     if os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb"):
         shutil.rmtree("~/.cache/huggingface/datasets/stanfordnlp___imdb")
 
     if os.path.isdir("~/tokenized_datasets/glue/gpt-2"):
         shutil.rmtree("~/tokenized_datasets/glue/gpt-2")
 
-    cola_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "gpt-2")
+    imdb_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "gpt-2")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert not cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert not imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
     assert os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb")
     assert os.path.isdir("~/tokenized_datasets/glue/gpt-2")
@@ -113,35 +113,35 @@ def test_create_cola_already_downloaded_gpt2():
     if os.path.isdir("~/tokenized_datasets/glue/gpt-2"):
         shutil.rmtree("~/tokenized_datasets/glue/gpt-2")
 
-    cola_dataset = IMDbLargeMovieReview(model_name = "gpt-2")
+    imdb_dataset = IMDbLargeMovieReview(model_name = "gpt-2")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
-def test_create_cola_already_downloaded_and_tokenized_gpt2():
+def test_create_imdb_already_downloaded_and_tokenized_gpt2():
     if os.path.isdir("~/.cache/huggingface/datasets/stanfordnlp___imdb"):
         shutil.rmtree("~/.cache/huggingface/datasets/stanfordnlp___imdb")
 
     if os.path.isdir("~/tokenized_datasets/glue/gpt-2"):
         shutil.rmtree("~/tokenized_datasets/glue/gpt-2")
 
-    cola_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "gpt-2")
+    imdb_dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "gpt-2")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert not cola_dataset.dataset_already_downloaded
-    assert not cola_dataset.dataset_already_tokenized
+    assert not imdb_dataset.dataset_already_downloaded
+    assert not imdb_dataset.dataset_already_tokenized
 
     assert os.path.isdir("~/.cache/huggingface/datasets/gpt-2")
     assert os.path.isdir("~/tokenized_datasets/glue/gpt-2")
 
-    cola_dataset = IMDbLargeMovieReview(model_name = "gpt-2")
+    imdb_dataset = IMDbLargeMovieReview(model_name = "gpt-2")
 
-    assert cola_dataset is not None
+    assert imdb_dataset is not None
 
-    assert cola_dataset.dataset_already_downloaded
-    assert cola_dataset.dataset_already_tokenized
+    assert imdb_dataset.dataset_already_downloaded
+    assert imdb_dataset.dataset_already_tokenized
 
 dataset: IMDbLargeMovieReview = IMDbLargeMovieReview(model_name = "bert")
