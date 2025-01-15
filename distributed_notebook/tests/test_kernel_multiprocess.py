aec961109f6e04575d3d05fccb81676c73181137
Ben <bcarver2@gmu.edu>
2025-01-15 21:20:06 +0000

    Working on multi-process pytorch tests

---
+++ distributed_notebook/tests/test_kernel_multiprocess.py
@@ -0,0 +1,9 @@
+def kernel_process():
+    pass
+
+
+def test_train_resnet_cifar10():
+    """
+    Create two processes, each of which houses a DistributedKernel that will train ResNet18 on CIFAR-10.
+    """
+    
