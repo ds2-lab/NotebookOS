ca5a6644e939bc5b85eb32446ef06bf667a05948
bcarver <bcarver2@gmu.edu>
2025-01-16 13:43:14 -0500

    Updated FIFO queue impl to be generic

--- common/queue/fifo.go
+++ common/queue/fifo.go
@@ -1,53 +1,55 @@
 package queue
 
-// FifoQueue implements a first-in first-out (FIFO) queue.
-type FifoQueue struct {
-	elements []interface{}
+// Fifo implements a first-in first-out (FIFO) queue.
+type Fifo[T any] struct {
+	elements []T
 }
 
-// NewQueue creates a new Queue struct with the specified initial size/capacity
+// NewFifo creates a new Queue struct with the specified initial size/capacity
 // and returns a pointer to it.
-func NewQueue(initialSize int) *FifoQueue {
+func NewFifo[T any](initialSize int) *Fifo[T] {
 	if initialSize < 0 {
 		initialSize = 1
 	}
 
-	return &FifoQueue{
-		elements: make([]interface{}, 0, initialSize),
+	return &Fifo[T]{
+		elements: make([]T, 0, initialSize),
 	}
 }
 
 // Enqueue adds the specified element to the queue.
-func (q *FifoQueue) Enqueue(elem interface{}) {
+func (q *Fifo[T]) Enqueue(elem T) {
 	q.elements = append(q.elements, elem)
 }
 
 // Dequeue removes and returns the next element in the queue.
 //
 // If the length of the Queue is 0, then Dequeue will return nil.
-func (q *FifoQueue) Dequeue() interface{} {
+func (q *Fifo[T]) Dequeue() (T, bool) {
 	if len(q.elements) == 0 {
-		return nil
+		var zero T
+		return zero, false
 	}
 
 	elem := q.elements[0]
 	q.elements = q.elements[1:]
 
-	return elem
+	return elem, true
 }
 
 // Peek returns but does not remove the next element in the queue.
 //
 // If the length of the Queue is 0, then Peek will return nil.
-func (q *FifoQueue) Peek() interface{} {
+func (q *Fifo[T]) Peek() (T, bool) {
 	if len(q.elements) == 0 {
-		return nil
+		var zero T
+		return zero, false
 	}
 
-	return q.elements[0]
+	return q.elements[0], true
 }
 
 // Len returns the number of elements in the queue.
-func (q *FifoQueue) Len() int {
+func (q *Fifo[T]) Len() int {
 	return len(q.elements)
 }
