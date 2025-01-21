b6a25b734cff84848bead12716d3f6eb9a9395ae
bcarver <bcarver2@gmu.edu>
2025-01-21 13:25:17 -0500

    Added helper script to change number of gpus

---
+++ deploy/docker-WSL2/set_num_gpus_per_host.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+########################################
+# Update Cluster Gateway configuration #
+########################################
+
+# File to modify
+target_file="./gateway/gateway.yml"
+
+# Ensure the file exists
+if [[ ! -f "$target_file" ]]; then
+  echo "Error: File '$target_file' does not exist."
+  exit 1
+fi
+
+# Ensure the argument is provided and valid
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <true|false>"
+  exit 1
+fi
+
+# Get the first argument and validate
+new_value=$1
+re='^[0-9]+$'
+if ! [[ $new_value =~ $re ]] ; then
+    echo "Error: Argument must be a positive integer."
+    exit 1
+fi
+
+# Use sed to update the file
+sed -i.bak -E "s/^use_real_gpus:.*/use_real_gpus: $new_value/" "$target_file"
+
+if [[ $? -eq 0 ]]; then
+  echo "Successfully updated 'use_real_gpus' to '$new_value' in '$target_file'."
+else
+  echo "Error: Failed to update the file."
+  exit 1
+fi
+
+#####################################
+# Update Local Daemon configuration #
+#####################################
+
+# File to modify
+target_file="./local_daemon/daemon.yml"
+
+# Ensure the file exists
+if [[ ! -f "$target_file" ]]; then
+  echo "Error: File '$target_file' does not exist."
+  exit 1
+fi
+
+# Ensure the argument is provided and valid
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <true|false>"
+  exit 1
+fi
+
+# Get the first argument and validate
+new_value=$1
+re='^[0-9]+$'
+if ! [[ $new_value =~ $re ]] ; then
+    echo "Error: Argument must be a positive integer."
+    exit 1
+fi
+
+# Use sed to update the file
+sed -i.bak -E "s/^use_real_gpus:.*/use_real_gpus: $new_value/" "$target_file"
+
+if [[ $? -eq 0 ]]; then
+  echo "Successfully updated 'use_real_gpus' to '$new_value' in '$target_file'."
+else
+  echo "Error: Failed to update the file."
+  exit 1
+fi
