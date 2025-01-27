d93722c42e45b0c96c66843e86c40aff651aced9
bcarver <bcarver2@gmu.edu>
2025-01-27 16:12:57 -0500

    Initial commit

--- gateway/daemon/submit.go
+++ gateway/daemon/submit.go
@@ -1,5 +1,21 @@
 package daemon
 
+import (
+	"github.com/Scusemua/go-utils/config"
+	"github.com/Scusemua/go-utils/logger"
+	"sync"
+)
+
 // ExecutionSubmitter ensures that "execute_request" (and "yield_request") messages are sent one-at-a-time.
 type ExecutionSubmitter struct {
+	mu  sync.Mutex
+	log logger.Logger
+}
+
+func NewExecutionSubmitter() *ExecutionSubmitter {
+	submitter := &ExecutionSubmitter{}
+
+	config.InitLogger(&submitter.log, submitter)
+
+	return submitter
 }
