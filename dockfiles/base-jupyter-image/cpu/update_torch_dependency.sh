27c0a9d05a9ac02a9f2c705fa8514fc078d79895
bcarver <bcarver2@gmu.edu>
2025-01-29 11:18:24 -0500

    Updated makefiles and script

--- dockfiles/base-jupyter-image/cpu/update_torch_dependency.sh
+++ dockfiles/base-jupyter-image/cpu/update_torch_dependency.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#!/bin/bash
-
-REQUIREMENTS_FILE="requirements.txt"
+REQUIREMENTS_FILE="distributed_notebooks_requirements.txt"
 NEW_TORCH_ENTRY="torch @ https://download.pytorch.org/whl/cpu-cxx11-abi/torch-2.5.1%2Bcpu.cxx11.abi-cp312-cp312-linux_x86_64.whl#sha256=0b55f1516410e4255132533b9f5a9621e48b7504d8adf22d927c57c9fa441bfd"
 
 # Replace the line starting with "torch" with the CPU-only dependency
