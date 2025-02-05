a88493ee58bb810a395c28dc52869089c6c92fa8
bcarver <bcarver2@gmu.edu>
2025-02-05 10:36:06 -0500

    Fixed install script for kernel

--- install_kernel_debug_py3-12-6.sh
+++ install_kernel_debug_py3-12-6.sh
@@ -5,5 +5,5 @@ PYTHON=/home/jovyan/Python-3.12.6/debug/python
 rm -rf dist build */*.egg-info *.egg-info
 # ${PYTHON} setup.py sdist
 # ${PYTHON} -m pip install dist/*.tar.gz
-# ${PYTHON} -m pip install -e .
+${PYTHON} -m pip install -e .
 ${PYTHON} -m distributed_notebook.kernel.install
