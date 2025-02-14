207f0e09bde05952410b18e44d45de896c4eecd3
bcarver <bcarver2@gmu.edu>
2025-02-14 15:12:45 -0500

    updated DeepSpeech model as well

--- distributed_notebook/deep_learning/models/speech/deep_speech.py
+++ distributed_notebook/deep_learning/models/speech/deep_speech.py
@@ -1,3 +1,4 @@
+import math
 import gc
 import time
 from typing import Optional, Dict, Any, Type
@@ -89,6 +90,8 @@ class DeepSpeech(DeepLearningModel):
         if target_training_duration_millis <= 0:
             return actual_training_time_millis, copy_cpu2gpu_millis, copy_gpu2cpu_millis
 
+        target_training_duration_sec: float = target_training_duration_millis / 1.0e3
+
         self.log.debug(f"Training for {target_training_duration_millis} milliseconds.")
 
         self.model.train()
@@ -107,14 +110,53 @@ class DeepSpeech(DeepLearningModel):
                 self._optimizer.zero_grad()
 
                 # Forward pass
-                forward_pass_start: float = time.time()
-                outputs = self.model(waveforms)
-                outputs = F.log_softmax(outputs, dim=2)
-                outputs = outputs.transpose(0, 1)  # (time, batch, n_class)
+                if self.gpu_available:
+                    output_st: float = time.time()
 
-                # Compute loss
-                loss = self._criterion(outputs, labels, input_lengths, label_lengths)
+                    outputs = self.model(waveforms)
+                    outputs = F.log_softmax(outputs, dim=2)
+                    outputs = outputs.transpose(0, 1)  # (time, batch, n_class)
+
+                    self.log.debug(f"\tComputed outputs in {round((time.time() - output_st)*1.0e3, 3):,} ms. "
+                                   f"Computing loss now.")
+
+                    loss_st: float = time.time()
+
+                    # Compute loss
+                    loss = self._criterion(outputs, labels, input_lengths, label_lengths)
+                else:
+                    sleep_interval_sec: float = target_training_duration_sec * 0.1
+
+                    self.log.warning(f"\tSimulating training because CUDA is unavailable. "
+                                     f"Sleeping for {round(sleep_interval_sec, 3):,f} seconds.")
+
+                    # Simulate computation time for a batch
+                    time.sleep(sleep_interval_sec)
+
+                    update_param_start: float = time.time()
+
+                    with torch.no_grad():  # Manually modify weights
+                        for param in self.model.parameters():
+                            # Random weight updates
+                            param.data = param.data + 0.01 * torch.randn_like(param)
+
+                    self.log.debug(f"\tUpdated parameters in {round((time.time() - update_param_start)*1.0e3, 3):,} ms.")
+
+                    last_dim: int = waveforms.shape[-1]
+
+                    # Simulate model outputs and labels
+                    dim: int = int(math.ceil(last_dim / 2)) # Fake predictions
+                    outputs = torch.randn(len(waveforms), dim, self._out_features, requires_grad=True)
+                    outputs = F.log_softmax(outputs, dim=2)
+                    outputs = outputs.transpose(0, 1)  # (time, batch, n_class)
+
+                    loss_st: float = time.time()
+
+                    # Compute simulated loss
+                    loss = self._criterion(outputs, labels, input_lengths, label_lengths)
+
                 loss.backward()
+                self.log.debug(f"\tComputed loss in {round((time.time() - loss_st) * 1.0e3, 3):,} ms: {loss.item()}")
 
                 self._optimizer.step()
                 forward_pass_end: float = time.time()
