2f6105d285fe66bd9f40b99d388e025165fdead5
Benjamin Richard Carver <bcarver2@gmu.edu>
2025-07-24 12:23:27 -0400

    Create build_images.sh
    Added a build_images.sh script

---
+++ setup/build_images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#################
+# scusemua/gopy #
+#################
+pushd "$GOPATH_ENV/pkg/gopy"
+python3.12 -m pip install pybindgen
+go install golang.org/x/tools/cmd/goimports@latest
+make 
+docker build -t $DOCKERUSER/gopy .
+popd
+
+pushd "$GOPATH_ENV/pkg/NotebookOS"
+pushd smr
+make build-linux-amd64
+popd
+make build-smr-linux-amd64
